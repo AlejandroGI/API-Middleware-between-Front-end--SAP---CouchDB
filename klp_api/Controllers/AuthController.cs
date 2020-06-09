7b94e7b12c370050a4af28f5dd96b8ba897811a3
Elias Becerra <ebecerra@itinnovation.cl>
2020-06-09 13:34:42 -0400

    base del proyecto

---
+++ klp_api/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using klp_api.Models;
+
+namespace klp_api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> Logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            Logger = logger;
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            HttpContext.Response.StatusCode = 200;
+            return new JsonResult(new GenericResponse
+            {
+                Message = "Login success",
+                Error = null,
+                Data = null
+            });
+        }
+    }
+}
---
+++ klp_api/Models/GenericResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace klp_api.Models
+{
+    public class GenericResponse
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("error")]
+        public string Error { get; set; }
+
+        [JsonProperty("data")]
+        public dynamic Data { get; set; }
+    }
+}

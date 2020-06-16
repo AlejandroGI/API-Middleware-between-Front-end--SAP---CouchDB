1e83a822a0202abb167306a7728de96a7f59a9a3
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-06-16 16:33:52 -0400

    estructura de métodos a tulizar

---
+++ klp_api/Controllers/CouchDBControllers/Categories.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace klp_api.Controllers.CouchDBControllers
+{
+    public class Categories
+    {
+        public void Categorie()
+        {
+
+        }
+        public void CategoriesProductCode()
+        {
+
+        }
+
+    }
+}

9e5e653f684a728d7b8a657ced0730014efcbd40
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-06-21 00:41:26 -0400

    SE OBTIENE CATEGORIA DE UN PRODUCTO

--- klp_api/Controllers/ReqControllers/CategoriesRequest.cs
+++ klp_api/Controllers/ReqControllers/CategoriesRequest.cs
@@ -1,14 +1,36 @@
-﻿namespace klp_api.Controllers.CouchDBControllers
+﻿using klp_api.Models.Req.Categories;
+using Newtonsoft.Json;
+
+namespace klp_api.Controllers.CouchDBControllers
 {
     public class CategoriesRequest
     {
-        public void Product()
+        public dynamic RequestCategoriesBody(int? groupCode, int? limit, int? skip)
         {
-
-        }
-        public void ProductCode()
-        {
-
+            if (groupCode == null)
+            {
+                groupCode = 101;
+            }
+            if (limit == null | skip == null)
+            {
+                limit = 10;
+                skip = 0;
+            }
+            ValidationCategoriesProductCodeReqBodyModel jsonObject = new ValidationCategoriesProductCodeReqBodyModel
+            {
+                selector = new CategoriesProductCodeReqBodyModel
+                {
+                    groupCode = new GroupCodeClass
+                    {
+                        eq = groupCode
+                    }
+                },
+                limit = limit,
+                skip = skip
+            };
+            dynamic json = JsonConvert.SerializeObject(jsonObject);
+            return json;
         }
     }
 }
+

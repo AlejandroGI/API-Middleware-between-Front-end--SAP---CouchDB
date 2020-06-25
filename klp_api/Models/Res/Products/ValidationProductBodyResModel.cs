a244be2f866ec92d5affa5752d033210908da6c6
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-06-25 09:53:51 -0400

    response de productos y por código

--- klp_api/Models/Res/Products/ValidationProductBodyResModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace klp_api.Models.Res
-{
-    public class ValidationProductsBodyResModel
-    {
-        public string rut { get; set; }
-        public Docs[] products { get; set; }
-        public string origin { get; set; }
-    }
-}
---
+++ klp_api/Models/Res/Products/ValidationProductsResBodyModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace klp_api.Models.Res.Products
+{
+    //~/api/products
+    public class ValidationProductsResBodyModel
+    {
+        [JsonProperty("rut")]
+        public string Rut { get; set; }
+        [JsonProperty("rut")]
+        public ProductsResBodyModel[] docs { get; set; }
+        [JsonProperty("bookmark")]
+        public string Bookmark { get; set; }
+    }
+    //~/api/products/:code
+
+    public class ValidationProductsCodeResBodyModel
+    {
+        [JsonProperty("rut")]
+        public string Rut { get; set; }
+        [JsonProperty("docs")]
+        public ProductsCodeResBodyModel[] Docs { get; set; }
+        [JsonProperty("bookmark")]
+        public string Bookmark { get; set; }
+    }
+
+}

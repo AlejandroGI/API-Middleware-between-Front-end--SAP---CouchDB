2b8b7f09f496950f98b72f76449c8fc2489efcab
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-06-30 18:26:25 -0400

    response model products

---
+++ klp_api/Controllers/ResController/CategoriesResponse.cs
@@ -0,0 +1,18 @@
+﻿using klp_api.Models.Res.Categories;
+
+namespace klp_api.Controllers.ResController
+{
+    public class CategoriesResponse
+    {
+        private ValidationCategoriesResBodyModel _jsonObject;
+        public dynamic ResponseProductsBody(dynamic res, string dataSource)
+        {
+            _jsonObject = new ValidationCategoriesResBodyModel
+            {
+                Docs = res.Docs,
+                Origin = dataSource
+            };
+            return _jsonObject;
+        }
+    }
+}
